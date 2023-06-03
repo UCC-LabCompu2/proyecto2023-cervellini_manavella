87f6e87e1720bc7b433ecd791d28ec9de245c761
pmanavella <2200292@ucc.edu.ar>
2023-06-03 14:11:12 -0300

    se muestran los resultados

--- funcionesProyecto.js
+++ funcionesProyecto.js
@@ -16,82 +16,65 @@ function calculoEncuentro() {
     //Cambiar la coma
     if (v1.includes(",")) {
         v1 = v1.replace(",", ".");
-    }...
+    }
+    if (v2.includes(",")) {
+        v2 = v2.replace("," , ".");
+    }
+    if (p1.includes(",")) {
+        p1 = p1.replace(",", ".");
+    }
+    if (p2.includes(",")) {
+        p2 = p2.replace(",", ".");
+    }
 
-    //Verificar is Nan
+    //Verificación de los datos ingresados en cada campo
     if(isNaN(v1)){
         alert("La velocidad 1 debe ser un número");
-        v1 = 0;
+        document.lasUnidades.input_velocidad1.value = "" ;
     }
-    if(){
-
-    }...
-
+    if(isNaN(v2)){
+        alert("La velocidad 2 debe ser un número");
+        document.lasUnidades.input_velocidad2.value = "" ;
+    }
+    if(isNaN(p1)){
+        alert("La posición 1 debe ser un número");
+        document.lasUnidades.input_posicion1.value = "" ;
+    }
+    if(isNaN(p2)){
+        alert("La posición 2 debe ser un número");
+        document.lasUnidades.input_posicion2.value = "" ;
+    }
 
     //Conversion de unidades
     if(u1==="kmh"){
         v1 = v1 * 100;
     }
-    if(){
-
+    if(u2==="kmh"){
+        v2 = v2 *100;
+    }
+    if(u1s==="km"){
+        p1 = p1*100;
+    }
+    if(u2s==="km"){
+        p2 = p2*100;
+    }
+    if(u1s==="cm"){
+        p1 = p1*100;
+    }
+    if(u2s==="cm"){
+        p2 = p2*100;
     }
 
-    //calculo
-
-
-    //asigno
-
-
-    console.log(v1, u1,p1,u1s,v2,u2,p2,u2s);
-    /*
-    // cartel alert si el valor ingresado no es válido
-    if (isNaN(valor)) {
-        alert("Se ingresó un valor inválido");
-        // vaciar campos
-        document.lasUnidades.input_velocidad1.value = "";
-        document.lasUnidades.input_posicion1.value = "";
-        document.lasUnidades.input_velocidad2.value = "";
-        document.lasUnidades.input_posicion2.value = "";
-    } else {
-        conversionUnidades();
-    }
-
-    if (valor.includes(",")) {
-        valor = valor.replace(",", ".");
-    }
-
-    var unidadPos1 = parseFloat(document.getElementById("input_unipri"));
-    var unidadVel1 = parseFloat(document.getElementById("input_unidad1"));
-    var unidadPos2 = parseFloat(document.getElementById("input_uniseg"));
-    var unidadVel2 = parseFloat(document.getElementById("input_unidadseg"));
-
-    velocidad1 = parseFloat(document.getElementById("input_velocidad1"));
-    velocidad2 = parseFloat(document.getElementById("input_velocidad2"));
-    posicion1 = parseFloat(document.getElementById("input_posicion1"));
-    posicion2 = parseFloat(document.getElementById("input_posicion2"));
-
-    // conversión de unidades para aquellas distintas a m o m/s
-    if (input_unidad1 != "m/s") {
-        velocidad_11 = convertirUnidades(velocidad1, input_unidad1);
-    }
-    if (input_unipri != "m") {
-        posicion_11 = convertirUnidades(posicion1, input_unipri);
-    }
-    if (input_unidadseg != "m/s") {
-        velocidad_22 = convertirUnidades(velocidad2, input_unidadseg);
-    }
-    if (input_uniseg != "m") {
-        posicion_22 = convertirUnidades(posicion2, input_uniseg);
-    }
-
+    //cálculo
     var tiempo;
-    tiempo = (posicion_11 - posicion_22) / (velocidad_22 - velocidad_11);
+    tiempo = (p1 - p2) / (v2 - v1);
     tiempo = tiempo * 3600;
-    distancia = posicion_11 + velocidad_11 * Number(tiempo);
+    distancia = p1 + v1 * Number(tiempo);
 
-    // mostrar resultados
+    // resultados en pantalla
     document.getElementById("resul_tiempo").value = tiempo + "  s";
     document.getElementById("resul_distancia").value = distancia + "  m";
-*/
+
+    console.log(v1, u1,p1,u1s,v2,u2,p2,u2s);
 
 }
