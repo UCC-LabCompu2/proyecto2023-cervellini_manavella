57e165bf60b99ce2c9f806b9edded6270b0e94c6
pmanavella <2200292@ucc.edu.ar>
2023-06-04 17:37:38 -0300

    cambio de imágenes canvas

--- funcionesProyecto.js
+++ funcionesProyecto.js
@@ -78,3 +78,29 @@ function calculoEncuentro() {
     console.log(v1, u1, p1, u1s, v2, u2, p2, u2s);
 
 }
+
+x = 0;
+dx = 2;
+
+function animarAuto() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var img = new Image();
+    img.src = "imagenes/autocanvas.png";
+
+    canvas.width = canvas.width;
+
+    img.onload = function () {
+        ctx.drawImage(img, x, 100);
+    }
+
+    if(x>canvas.width) {
+        x = 0;
+    }
+    x+=dx;
+}
+
+
+
+
